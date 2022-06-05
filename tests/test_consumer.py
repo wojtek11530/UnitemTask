c7da18a6caf959ab748d2a21cb17dbad5be5bb40
Wojciech Korczyński <wojtek19962a32@gmail.com>
2022-06-05 23:50:36 +0200

    feat: implement Consumer

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -13,7 +13,7 @@ class ConsumerTestCase(unittest.TestCase):
         items_in_queue = 15
         in_queue = Queue()
         for i in range(items_in_queue):
-            in_queue.put(1)
+            in_queue.put(i + 1)
 
         out_queue = Queue()
         stop_event = Event()
@@ -23,8 +23,8 @@ class ConsumerTestCase(unittest.TestCase):
 
         with patch("unitem_task.consumer.Consumer.process_data") as mock:
             mock.return_value = 1
-            consumer.start()
             stop_event.set()
+            consumer.start()
             consumer.join()
 
         data_put_into_queue_counter = 0
@@ -35,8 +35,8 @@ class ConsumerTestCase(unittest.TestCase):
         self.assertEqual(data_put_into_queue_counter, items_in_queue)
 
     def test_shape_of_process_data(self):
-        input_shape = (3, 1000, 500)
-        rows, cols, channels = (1000, 600, 3)
+        input_shape = (600, 800, 3)
+        rows, cols, channels = input_shape
         img = np.random.randint(
             256,
             size=rows * cols * channels,
@@ -46,5 +46,5 @@ class ConsumerTestCase(unittest.TestCase):
         out_img = Consumer.process_data(img)
         out_rows, out_cols, out_channels = out_img.shape
 
-        self.assertEqual(out_rows, 500)
         self.assertEqual(out_rows, 300)
+        self.assertEqual(out_cols, 400)
--- unitem_task/consumer.py
+++ unitem_task/consumer.py
@@ -1,16 +1,28 @@
 import threading
 from queue import Queue
 
+import cv2
 import numpy as np
 
 
 class Consumer(threading.Thread):
     def __init__(self, in_queue: Queue, out_queue: Queue, stop_event: threading.Event):
         super().__init__()
+        self._in_queue = in_queue
+        self._out_queue = out_queue
+        self._stop_event = stop_event
 
     def run(self) -> None:
-        pass
+        while not (self._in_queue.empty() and self._stop_event.is_set()):
+            img = self._in_queue.get()
+            processed_img = self.process_data(img)
+            self._out_queue.put(processed_img)
 
     @staticmethod
     def process_data(img: np.ndarray) -> np.ndarray:
-        pass
+        width = int(img.shape[1] / 2)
+        height = int(img.shape[0] / 2)
+        dim = (width, height)
+        resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+        img_blurred = cv2.medianBlur(resized, 5)
+        return img_blurred
